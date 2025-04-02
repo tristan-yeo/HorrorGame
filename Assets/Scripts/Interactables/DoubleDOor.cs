80c4221e93623fc3ccc81d06ac0af49803301703
nadakhn <109417597+nadakhn@users.noreply.github.com>
2025-04-02 16:43:13 +0800

    fix-walls-doors (#10)
    * fix broken walls from ryan
    
    * add auto swing double door
    
    * layer cleanup
    
    * fix door bug
    
    * change single door asset
    
    * single door animation
    
    * fix walls in waiting area
    
    * fix some more walls but not all
    
    * new scene for nada to edit

---
+++ Assets/Scripts/Interactables/DoubleDOor.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class DoubleDoor : MonoBehaviour
+{
+    [SerializeField] private Animator door = null;
+    [SerializeField] private AudioSource audioSource = null;
+    [SerializeField] private AudioClip doorSound;
+    [SerializeField] private AudioClip doorCloseSound;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            door.Play("DoorOpen", 0, 0.0f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            door.Play("DoorClose", 0, 0.0f);
+        }
+    }
+
+    public void PlayDoorSound()
+    {
+        if (audioSource != null && doorSound != null)
+        {
+            audioSource.PlayOneShot(doorSound);
+        }
+    }
+
+    public void PlayDoorCloseSound()
+    {
+        if (audioSource != null && doorCloseSound != null)
+        {
+            audioSource.PlayOneShot(doorCloseSound);
+        }
+    }
+
+
+
+}
